56aa2bd4ee0a1490feaffafa44d64f6630809ea5
chengfeng30121 <122871550+chengfeng30121@users.noreply.github.com>
2023-09-17 05:12:18 +0000

    modified:   example/views.py

--- example/views.py
+++ example/views.py
@@ -27,5 +27,17 @@ def ip_api(request):
 def tmp(request):
     filename = '/var/task/django/db/backends/sqlite3/_functions.py'
     with open(filename, mode='r') as f:
-        content = f.read().replace('\n', '</br>').replace(' ', '&nbsp;')
+        content = f.read()
+        i = 1
+        c = ''
+        for a in content.split('\n'):
+            str(i)
+            if i < 10:
+                '00' + i
+            elif i > 10 and i < 100:
+                '0' + i
+            else:
+                i = i
+            c = c + i + ' ' + a + '\n'
+        content = c.replace('\n', '</br>').replace(' ', '&nbsp;')
     return HttpResponse(content)
